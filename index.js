ff34ed171c4f19d175ea13c93e5cccf9c154a31b
tvhanh <hanhvan2502@gmail.com>
2025-04-08 13:34:57 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/hello", (req, res) => {
+  res.send("Welcome to Express deployed on Render!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT", PORT);
+});
