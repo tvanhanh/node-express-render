d9ef8c392722f9709ec81d3a39a7ed5b39e7a0c4
tvhanh <hanhvan2502@gmail.com>
2025-04-09 22:17:13 +0700

    Chỉnh sửa file index.j322s 2

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const validator = require("email-validator");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
   res.send("Welcome to Express deployed on Render!");
 });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my Express app on Render!");
+});
+
+app.post("/emailValidation", (req, res) => {
+  const postData = req.body;
+  if (postData.email) {
+    res.json({ status: validator.validate(postData.email) });
+  } else {
+    res.status(400).json({ status: "wrong request input" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on PORT", PORT);
 });
-app.get("/", (req, res) => {
-    res.send("Welcome to my Express app on Render!");
-  });
-  app.get("/emailValidation", (req, res) => {
-    const postData = req.body;
-    if(postData.email){
-       res.json({'status': validator.validate(postData.email)});
-
-    } else {
-      res.status(500).json({'status': 'wrong request input'})
-    }
-  });
-  
