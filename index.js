14bb5ea2d443e7f85d879f5c1e698ef054537ed6
tvhanh <hanhvan2502@gmail.com>
2025-04-09 22:03:47 +0700

    Chỉnh sửa file index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const validator = require("email-validator");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
   res.send("Welcome to Express deployed on Render!");
@@ -12,5 +16,14 @@ app.listen(PORT, () => {
 });
 app.get("/", (req, res) => {
     res.send("Welcome to my Express app on Render!");
+  });
+  app.get("/emailValidation", (req, res) => {
+    const postData = req.body;
+    if(postData.email){
+       res.json({'status': validator.validate(postData.email)});
+
+    } else {
+      res.status(500).json({'status': 'wrong request input'})
+    }
   });
   
