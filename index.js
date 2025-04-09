4216aa88c1cbd6beb67f75e5bbb8aad30051f788
tvhanh <hanhvan2502@gmail.com>
2025-04-09 22:40:11 +0700

    log

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
   res.send("Welcome to Express deployed on Render!");
+  console.info('GET /hello success')
 });
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Express app on Render!");
+
 });
 
 app.post("/emailValidation", (req, res) => {
   const postData = req.body;
   if (postData.email) {
+    console.info('POST /emailValidation success')
     res.json({ status: validator.validate(postData.email) });
   } else {
+    console.warn('POST /emailValidation wrong input')
     res.status(400).json({ status: "wrong request input" });
   }
 });
